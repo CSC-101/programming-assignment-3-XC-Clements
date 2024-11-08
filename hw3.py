aba5ea6c6fa136fa5683ad95926286cd86d16950
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-11-08 09:43:40 -0800

    11/8/24 9:43

--- hw3.py
+++ hw3.py
@@ -20,3 +20,62 @@ def filter_by_state(county_list:list[CountyDemographics], state:str) -> list[Cou
     return output_list
 
 print(filter_by_state(data_imports.reduced_data_set, "CA"))
+
+#Part 3
+def population_by_education(county_list:list[CountyDemographics], education_level:str) -> float:
+    output = 0
+    for n in county_list:
+        if education_level in n.education:
+            output += (n.education[education_level] / 100) * n.population['2014 Population']
+        else:
+            return 0.0
+    return output
+
+print(population_by_education(data_imports.reduced_data_set, 'High School or Higher'))
+
+def population_by_ethnicity(county_list:list[CountyDemographics], ethnicity:str) -> float:
+    output = 0
+    for n in county_list:
+        if ethnicity in n.ethnicities:
+            output += (n.ethnicities[ethnicity] / 100) * n.population['2014 Population']
+        else:
+            return 0.0
+    return output
+
+print(population_by_ethnicity(data_imports.reduced_data_set, 'Asian Alone'))
+
+def population_below_poverty_level(county_list:list[CountyDemographics]) -> float:
+    output = 0
+    for n in county_list:
+        output += (n.income['Persons Below Poverty Level'] / 100) * n.population['2014 Population']
+    return output
+
+print(population_below_poverty_level(data_imports.reduced_data_set))
+
+#Part 4
+def percent_by_education(county_list:list[CountyDemographics], education_level:str) -> float:
+    total_pop = 0
+    for n in county_list:
+        total_pop += n.population['2014 Population']
+    return (population_by_education(county_list, education_level) / total_pop) * 100
+
+print(percent_by_education(data_imports.reduced_data_set, 'High School or Higher'))
+
+def percent_by_ethnicity(county_list:list[CountyDemographics], ethnicity:str) -> float:
+    total_pop = 0
+    for n in county_list:
+        total_pop += n.population['2014 Population']
+    total_percent = (population_by_ethnicity(county_list, ethnicity)/ total_pop) * 100
+    return total_percent
+
+print(percent_by_ethnicity(data_imports.reduced_data_set, 'Asian Alone'))
+
+def percent_below_poverty_level(county_list:list[CountyDemographics], education_level:str) -> float:
+    total_pop = 0
+    for n in county_list:
+        total_pop += n.population['2014 Population']
+    return (population_below_poverty_level(county_list) / total_pop) * 100
+
+print(percent_below_poverty_level(data_imports.reduced_data_set, 'High School or Higher'))
+
+#Part 5
